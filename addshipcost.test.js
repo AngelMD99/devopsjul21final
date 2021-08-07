e34bf79eb6fc19a3841ac3b6927d3bbc0adb746c
AngelMD99 <angelm.duarte@hotmail.com>
2021-08-07 12:40:30 -0600

    Correción sintaxis test
    Se corrige la sintaxis del código test ya que los paréntesis estaban incorrectos y marcaban error la prueba.

--- addshipcost.test.js
+++ addshipcost.test.js
@@ -1,11 +1,13 @@
 total = require("./addshipcost")
 
 test('verificar suma de costo de envío', ()=> {
-    expect(total(240).toBe(265))
+    expect(total(240)).toBe(265)
+    
+    
 
 } )
 
 test('verificar suma de costo de envío, prueba 2', ()=> {
-    expect(total(255).toBe(270))
+    expect(total(245)).toBe(270)
 
 } )
